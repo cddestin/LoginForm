6e957364bf9d63639376030efeaa52670e319fd8
cddestin <48738687+cddestin@users.noreply.github.com>
2019-08-15 07:12:10 -0400

    added files

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+    
+// dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://Buttlicker:t2_5RBinXLSc!qd@cluster0-pytwy.mongodb.net/test?retryWrites=true&w=majority';
+module.exports = {
+    mongoURI: dbPassword
+};
